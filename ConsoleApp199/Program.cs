fa9586f570a15a3d54c8818c860b17d7c4bf4ca4
Никита <153456515+Lairont@users.noreply.github.com>
2023-12-14 18:51:11 +0300

    Add files via upload

---
+++ ConsoleApp199/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp199
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание1
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    x += 1;
+            //}
+            //else
+            //{
+            //    x -= 2;
+            //}
+            //Console.WriteLine($"{x}");
+            //Console.ReadLine();
+            //Задание 2
+            //Console.Write("Введите первое число: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int b = int.Parse(Console.ReadLine());
+            //if (x != b)
+            //{
+            //    a = a+b;
+            //    b = a+b;
+            //}
+            //else
+            //{
+            //    a = 0;
+            //    b = 0;
+            //}
+            //Console.WriteLine($"{a}\n{b}");
+            //Console.ReadLine();
+            //Задание 3
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    x += 1;
+            //}
+            //else if (x < 0)
+            //{
+            //    x -= 2;
+            //}
+            //else
+            //{
+            //    x = 10;
+            //}
+            //Console.WriteLine($"{x}");
+            //Console.ReadLine();
+            //Задание 4
+            //Console.Write("x = ");
+            //int x = int.Parse(Console.ReadLine());
+            //double z = 0;
+            //double result = x >= 0 ? z = 2 + 3 * Math.Sqrt(x) : z = x - 2 * Math.Pow(x, 2);
+            //Console.WriteLine($"z = {z:f2}");
+            //Console.ReadLine();
+            //Задание 5
+            //Console.Write("Введите x: ");
+            //double x = double.Parse(Console.ReadLine());
+            //if (0 < x && x < 5)
+            //{
+            //    x = 2 * x;
+            //}
+            //else if (5 < x && x < 10)
+            //{
+            //    x = x;
+            //}
+            //else
+            //{
+            //    x = 0;
+            //}
+            //Console.WriteLine($"x = {x}");
+            //Console.ReadLine();
+        }
+    }
+}
